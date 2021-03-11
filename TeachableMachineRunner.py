71986895e0f5b6fac123be67842b2867eed98759
Donghoon Lee <movehoon@gmail.com>
2021-03-11 20:49:39 +0900

    Add tm_model class

--- TeachableMachineRunner.py
+++ TeachableMachineRunner.py
@@ -1,29 +1,61 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit)
-
+from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit, QPushButton, QFileDialog)
+from PyQt5.QtGui import QPixmap
+from PIL import Image, ImageOps
+import tm_model
 
 class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.tm = tm_model.TM_Model('keras_model.h5')
+        # image = Image.open('cat.jpg')
+        # self.tm.predict(image)
 
     def initUI(self):
         grid = QGridLayout()
         self.setLayout(grid)
 
-        grid.addWidget(QLabel('Load Model:'), 0, 0)
-        grid.addWidget(QLabel('Author:'), 1, 0)
-        grid.addWidget(QLabel('Review:'), 2, 0)
+        # Line1. Keras Model Open
+        btnModelOpen = QPushButton('Model Open', self)
+        btnModelOpen.clicked.connect(self.modelOpen)
+        # Line2. Image Open
+        btnImageOpen = QPushButton('Image Open', self)
+        btnImageOpen.clicked.connect(self.imageOpen)
+        # Line3. Image Show
+        self.imageLabel = QLabel()
+        # Line4. Show Result
+        self.textResult = QTextEdit()
 
-        grid.addWidget(QLineEdit(), 0, 1)
-        grid.addWidget(QLineEdit(), 1, 1)
-        grid.addWidget(QTextEdit(), 2, 1)
+        grid.addWidget(QLabel('Load Model:'), 0, 0)
+        grid.addWidget(QLabel('Open Image:'), 1, 0)
+        grid.addWidget(QLabel('Review:'), 2, 0)
+        grid.addWidget(QLabel('Result:'), 3, 0)
+
+        grid.addWidget(btnModelOpen, 0, 1)
+        grid.addWidget(btnImageOpen, 1, 1)
+        grid.addWidget(self.imageLabel, 2, 1)
+        grid.addWidget(self.textResult, 3, 1)
 
         self.setWindowTitle('QGridLayout')
         self.setGeometry(300, 300, 300, 200)
         self.show()
 
+    def modelOpen(self):
+        model_path = QFileDialog.getOpenFileName(self, 'Open Model', './')
+        print(model_path)
+        if len(model_path[0]):
+            self.tm = tm_model.TM_Model(model_path[0])
+
+    def imageOpen(self):
+        image_path = QFileDialog.getOpenFileName(self, 'Open Image', './')
+        print(image_path)
+        if len(image_path[0]):
+            self.imageLabel.setPixmap(QPixmap(image_path[0]))
+            image = Image.open(image_path[0])
+            self.textResult.setText(str(self.tm.predict(image)))
+            # print('result: ', result)
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
---
+++ tm_model.py
@@ -0,0 +1,54 @@
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+######
+# Teachable Machine Model
+# Required python v3.7.10
+# Required tensorflow v2.
+#####
+class TM_Model:
+
+
+    def __init__(self, model_path):
+
+        # Disable scientific notation for clarity
+        np.set_printoptions(suppress=True)
+
+        # Load the model
+        self.model = tensorflow.keras.models.load_model(model_path)
+
+        # Create the array of the right shape to feed into the keras model
+        # The 'length' or number of images you can put into the array is
+        # determined by the first position in the shape tuple, in this case 1.
+        self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+
+    def predict(self, image):
+        # Replace this with the path to your image
+        # image = Image.open('dog.jpg')
+
+        #resize the image to a 224x224 with the same strategy as in TM2:
+        #resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+        #turn the image into a numpy array
+        image_array = np.asarray(image)
+
+        # display the resized image
+        # image.show()
+
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+        # Load the image into the array
+        self.data[0] = normalized_image_array
+
+        # run the inference
+        prediction = self.model.predict(self.data)
+        print(prediction[0])
+        # print('max value: ', max(prediction[0]))
+        # print('max index: ', prediction[0].index(max(prediction[0])))
+        return np.argmax(prediction)
+        # return prediction[0].index(max(predictionp[0]))
